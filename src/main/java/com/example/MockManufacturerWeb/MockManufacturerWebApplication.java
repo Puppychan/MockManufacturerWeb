d3a70e36593425eaba53c86173e76517cd35c0cb
Matsuri <nhungmaitran1412@gmail.com>
2022-05-17 12:27:26 +0700

    Initial commit

---
+++ src/main/java/com/example/MockManufacturerWeb/MockManufacturerWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.MockManufacturerWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockManufacturerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockManufacturerWebApplication.class, args);
+	}
+
+}
